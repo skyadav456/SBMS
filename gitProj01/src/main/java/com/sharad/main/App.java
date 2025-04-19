eebae61d36b89ed897348f7abb1ff058c965b902
skyadav456 <sharadk4545@gmail.com>
2025-04-19 08:06:35 +0530

    initial commit

---
+++ gitProj01/src/main/java/com/sharad/main/App.java
@@ -0,0 +1,13 @@
+package com.sharad.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
